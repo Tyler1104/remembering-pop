5dbdc1ec541429aed53929befd1899e18e997c8c
Tyler1104 <Tyler1104@users.noreply.github.com>
2025-07-14 14:24:55 +0000

    deploy: 388c5d138999092de06d9475183872e2ca55337a

--- _nuxt/static/1752502917/charity-donation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1752502917",layout:"default",error:a,serverRendered:true,routePath:"\u002Fcharity-donation",config:{name:"Patrick Shaun Riley",dates:"May 19th 1955 - July 10th 2025",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
